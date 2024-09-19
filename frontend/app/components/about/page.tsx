02289a4e1528a42c165a3e5fcf7a303114f41134
minsukim2827 <m.kim006064@gmail.com>
2024-09-19 20:00:55 +1200

    added libraries and testing framework

---
+++ frontend/app/components/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is the about page of our application.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
--- frontend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-Hello world
-    </main>
-  );
-}
