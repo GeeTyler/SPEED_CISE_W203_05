5a20f2d31bd798144cf5c54eff397ac4c74d065f
minsukim2827 <m.kim006064@gmail.com>
2024-09-30 00:25:45 +1300

    implemented US-009

--- frontend/app/components/analyst-dashboard/page.tsx
+++ frontend/app/components/analyst-dashboard/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+
+import React from 'react';
+import ArticlesTable from './_components/ArticlesTable';
+import { useAnalystArticles } from '@/app/hooks/useAnalystArticles';
+
 const AnalystDashboard: React.FC = () => {
-    return (
-        <div>
-            <h1>Analyst Dashboard</h1>
-        </div>
+    const { analystArticles, fetchAnalystArticles } = useAnalystArticles();
+
+    const handleArticleUpdate = () => {
+        fetchAnalystArticles();
+      };
+      return (
+        <main className="p-8">
+          <h1 className="text-2xl m-4">Analyst Queue Articles</h1>
+          <ArticlesTable
+            articles={analystArticles}
+            showActions={true}
+            onArticleUpdate={handleArticleUpdate}
+          />
+
+        </main>
     );
 };
 
 export default AnalystDashboard;
+
